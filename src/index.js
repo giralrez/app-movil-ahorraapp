48ae07d6b9106d1c4186e8685da16c0945bb9e81
andre <andre@4Ndr3s.localdomain>
2026-08-20 21:33:46 -0500

    feat: reubicar App.js y extraer AppRoutes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app/App';
 
 // Estilos base OBLIGATORIOS de Ionic
 import '@ionic/react/css/core.css';
